2399943b3e9790a5adee8bf0d495a0f58bcb940e
suoke123 <asak222@hanmail.net>
2021-04-13 14:05:42 +0900

    0413_btnArrow_Slide

---
+++ script/script.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    //1. 정방향 슬라이드 적용( 버튼기호 ">", Next )
+
+    $("#btnNext>span").click(function(){
+        $("#shuttleFrame").animate(
+            {"margin-left": "-100px"},
+            800,
+            function() {
+                $("#shuttleFrame").css({"margin-left": "0"});
+                $(".slideBox:first-child").insertAfter(".slideBox:last-child");
+            }
+        );
+    });
+
+    //2. 정방향 슬라이드 적용( 버튼기호 "<", Prev )
+
+    $("#btnPrev>span").click(function(){
+        $(".slideBox:last-child").insertBefore(".slideBox:first-child");
+        $("#shuttleFrame").css({"margin-left": "-100px"});
+
+        $("#shuttleFrame").animate(
+            {"margin-left": "0"},
+            800
+        );
+    });
+
+});
